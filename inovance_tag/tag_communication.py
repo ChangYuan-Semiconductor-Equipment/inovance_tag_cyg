849c227134223f6e81d5b6b6f5be1801d3ae5f38
LiuWei <183074632@qq.com>
2024-10-25 16:16:02 +0800

    feat: remove invalid function and add data type

--- inovance_tag/tag_communication.py
+++ inovance_tag/tag_communication.py
@@ -15,7 +15,7 @@ class TagCommunication:
     dll_path = f"{os.path.dirname(__file__)}/inovance_tag_dll/TagAccessCS.dll"
 
     def __init__(self, plc_ip):
-        clr.AddReference(TagCommunication.dll_path)
+        clr.AddReference(self.dll_path)
         from TagAccessCS import TagAccessClass
         self._tag_instance = TagAccessClass()
         self._plc_ip = plc_ip
@@ -80,28 +80,6 @@ class TagCommunication:
         except Exception as exc:
             raise PLCReadError(f"Read failure: may be not connect plc {self.ip}") from exc
 
-    def execute_read_struct(self, tag_name: str, save_log=True):
-        """ Read the value of the specified tag name of struct.
-
-        Args:
-            tag_name (str): Tag name to be read of struct.
-            save_log (bool): Do you want to save the log? Default save.
-
-        Raises:
-            PLCReadError: An exception occurred during the reading process.
-        """
-        try:
-            if (handle := self.handles.get(tag_name)) is None:
-                handle = self.tag_instance.CreateTagHandle(tag_name)[0]
-                self.handles.update({tag_name: handle})
-            save_log and self.logger.info(f"*** Start read {tag_name} value ***")
-            result, state = self.tag_instance.ReadStructField(handle)
-            save_log and self.logger.info(f"*** End read {tag_name}'s value *** -> "
-                                          f"value_type: TC_STRUCT, value: {result}, read_state: {state.ToString()}")
-            return result
-        except Exception as exc:
-            raise PLCReadError(f"*** Read failure: may be not connect plc {self.ip}") from exc
-
     def execute_write(self, tag_name: str, write_type: str, value: Union[int, bool, str], save_log=True):
         """ Write data of the specified type to the designated tag location.
 
@@ -125,34 +103,6 @@ class TagCommunication:
             save_log and self.logger.info(f"*** Start write {tag_name} value *** -> value_type: "
                                           f"{write_type}, value: {value}")
             result = self.tag_instance.WriteTag(handle, value, getattr(self.tag_instance.TagTypeClass, write_type))
-            save_log and self.logger.info(f"*** End write {tag_name}'s value *** -> write_state: {result.ToString()}")
-            if result == self.tag_instance.TAResult.ERR_NOERROR:
-                return True
-            return False
-        except Exception as exc:
-            raise PLCWriteError(f"*** Write failure: may be not connect plc {self.ip}") from exc
-
-    def execute_write_struct(self, tag_name: str, value: Union[int, bool, str], save_log=True):
-        """ Write data of the struct type to the designated tag location.
-
-        Args:
-            tag_name (str): Tag name to be written with value.
-            value (Union[int, bool, str]): Write value.
-            save_log (bool): Do you want to save the log? Default save.
-
-        Returns:
-            bool: Is the writing successful.
-
-        Raises:
-            PLCWriteError: An exception occurred during the writing process.
-        """
-        try:
-            if (handle := self.handles.get(tag_name)) is None:
-                handle = self.tag_instance.CreateTagHandle(tag_name)[0]
-                self.handles.update({tag_name: handle})
-            save_log and self.logger.info(f"*** Start write {tag_name} value *** -> value_type: "
-                                          f"TC_STRUCT, value: {value}")
-            result = self.tag_instance.WriteStructField(handle, value)
             save_log and self.logger.info(f"*** End write {tag_name}'s value *** -> write_state: {result.ToString()}")
             if result == self.tag_instance.TAResult.ERR_NOERROR:
                 return True
